3308df86634271bc0a945525d4b5323123ba44b4
joseph_lee2633 <joseph_lee2633@163.com>
2017-08-18 17:15:00 +0800

    task jump into eachother ok

--- MyOS/main.c
+++ MyOS/main.c
@@ -9,30 +9,92 @@
 
 /* Private variables ---------------------------------------------------------*/
 /* Private functions ---------------------------------------------------------*/
-void task1()
-{
-    int i=0;
-    while(1){
-        i++;
-    }
-}
+typedef void (*taskfunc)( void );
+typedef unsigned int stack_st;
+
 void trigger_svc()
 {
     __asm("svc 0");
 }
 typedef struct
 {
-    unsigned int *cur_sp;
-    unsigned int task_sp[0xFF];
+    stack_st *cur_sp;
+    stack_st task_sp[256];
 }tcb;
 
-tcb task1_tcb;
-tcb *cur_task1_tcb;
+tcb task1_tcb, task2_tcb;
+tcb *cur_task_tcb;
+
+void task1();
+void task2();
+int init_task_stack(tcb *ptask_tcb,unsigned int stack_size,taskfunc taskfun);
+
+void task1()
+{
+    int i=0;
+    while(i<3){
+        i++;
+    }
+    i=0;
+    init_task_stack(&task2_tcb,sizeof(task2_tcb.task_sp),task2); 
+    cur_task_tcb=&task2_tcb;
+    trigger_svc();
+}
+void task2()
+{
+    int i=0;
+    while(i<3){
+        i++;
+    }
+    i=0;
+    init_task_stack(&task1_tcb,sizeof(task1_tcb.task_sp),task1);
+    cur_task_tcb=&task1_tcb;
+    trigger_svc();    
+}
+void task_exit()
+{
+
+}
+
 /* Main program */
 #define portINITIAL_XPSR                ( 0x01000000 )
+
+int init_task_stack(tcb *ptask_tcb,unsigned int stack_size,taskfunc taskfun)
+{
+    int i;
+    stack_st *task_sp_tmp=ptask_tcb->task_sp;
+    for(i=0;i<stack_size/sizeof(stack_st);i++){
+        *task_sp_tmp++=0;
+    }
+    ptask_tcb->cur_sp=task_sp_tmp-1;
+    *ptask_tcb->cur_sp=portINITIAL_XPSR;
+    ptask_tcb->cur_sp--;
+    *ptask_tcb->cur_sp=(unsigned int)taskfun;
+    ptask_tcb->cur_sp--;
+    *ptask_tcb->cur_sp=(unsigned int)task_exit;
+    ptask_tcb->cur_sp--;
+#ifdef DEBUG_STACK
+    *ptask_tcb->cur_sp=12;
+    ptask_tcb->cur_sp--;
+    *ptask_tcb->cur_sp=3;
+    ptask_tcb->cur_sp--;
+    *ptask_tcb->cur_sp=2;
+    ptask_tcb->cur_sp--;
+    *ptask_tcb->cur_sp=1;
+    ptask_tcb->cur_sp--;
+    *ptask_tcb->cur_sp=0xFFFFFFF0;
+#else
+    ptask_tcb->cur_sp-=4;
+#endif
+    return 0;
+}
 int main(void)
 {
-    int ret=0,i;
+    int ret=0;
+    ret=init_task_stack(&task1_tcb,sizeof(task1_tcb.task_sp),task1);
+    ret=init_task_stack(&task2_tcb,sizeof(task2_tcb.task_sp),task2);  
+#if 0
+    int i;
     //rr_basic_init();
     for(i=0;i<sizeof(task1_tcb.task_sp)/sizeof(task1_tcb.task_sp[0]);i++){
         task1_tcb.task_sp[i]=0;
@@ -44,7 +106,8 @@ int main(void)
     task1_tcb.cur_sp--;
     *task1_tcb.cur_sp=0;
     task1_tcb.cur_sp-=5;
-    cur_task1_tcb=&task1_tcb;
+#endif
+    cur_task_tcb=&task1_tcb;
     trigger_svc();
     return ret;
 }
--- MyOS/my_tiny_port.s
+++ MyOS/my_tiny_port.s
@@ -4,11 +4,11 @@
     PRESERVE8
     
     ;PUBLIC MY_SVC_Handler
-    IMPORT cur_task1_tcb
+    IMPORT cur_task_tcb
     EXPORT MY_SVC_Handler
 
 MY_SVC_Handler:
-    ldr	r3, =cur_task1_tcb
+    ldr	r3, =cur_task_tcb
     ldr	r2, [r3]
     ldr r1, [r2]
     msr msp, r1
